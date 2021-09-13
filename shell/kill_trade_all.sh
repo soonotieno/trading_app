a2a930072b1a988664236e956d8ff9f0eea75e72
JongSoon An <soonotieno@gmail.com>
2021-09-13 20:03:58 +0900

    changed

--- shell/kill_trade_all.sh
+++
@@ -1,1 +0,0 @@
-ps -ef | grep python | grep robobits |awk '{print $2}' | xargs kill -9
--- test.py
+++
@@ -1,9 +0,0 @@
-def print_human(name, age, job):
-    print('*human:', name, age, job)
-
-yourname = 'john'
-yourage = '24'
-yourjob = 'salesman'
-print_human(yourname, yourage, yourjob)
-
-
